5614e602d9518f1147a7d2153ce4e51e6a71a1bf
tiagobani <tiago.bani@yahoo.com.br>
2020-09-11 04:46:51 -0300

    [feat]: add statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    </>
+  );
 };
 
 export default App;
