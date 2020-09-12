1134d910487c6e7586ff2d4b32f2fd409f3fdd31
tiagobani <tiago.bani@yahoo.com.br>
2020-09-12 08:20:08 -0300

    [feat]: add components button and input

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,16 +1,56 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
-import { Container, Title } from './styles';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import {
+  Container,
+  Title,
+  ForgotPassowrd,
+  ForgotPassowrdText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SingIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
-    </Container>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => {}}>Entrar</Button>
+
+            <ForgotPassowrd onPress={() => {}}>
+              <ForgotPassowrdText>Esqueci minha senha.</ForgotPassowrdText>
+            </ForgotPassowrd>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9900" />
+        <CreateAccountButtonText>Criar uma conta.</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
