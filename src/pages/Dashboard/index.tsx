bfb2beb379d82595b55c21bf6753c26b58250384
tiagobani <tiago.bani@yahoo.com.br>
2020-10-20 05:00:16 -0300

    [feat]: add header of dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
-import Button from '../../components/Button';
+import { useNavigation } from '@react-navigation/native';
+import { useCallback } from 'react';
+// import { View } from 'react-native';
 import { useAuth } from '../../hooks/auth';
-
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  const { singOut } = useAuth();
+  const { singOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button onPress={singOut}>Sair</Button>
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo,{'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
